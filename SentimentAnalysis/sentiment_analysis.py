c239813e7fcd00955ca3d32d29415c814621162e
Utkarsh Tiwari <tiwariut2000@gmail.com>
2025-07-19 23:59:43 +0530

    Sentiment Analysis NLP package

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+# def <function_name>(<input_args>):
+#     url = '<relevant_url>'
+#     headers = {<header_dictionary>}
+#     myobj = {<input_dictionary_to_the_function>}
+#     response = requests.post(url, json = myobj, headers=headers)
+#     formatted_response = json.loads(response.text)
+#     label = formatted_response['documentSentiment']['label']
+#     score = formatted_response['documentSentiment']['score']
+#     return {'label': label, 'score': score}
